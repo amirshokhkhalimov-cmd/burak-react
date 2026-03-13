1bba18abd546047356057f03a3261c73690a5346
Amirshokh Khalimov <amirshokhkhalimov@gmail.com>
2026-03-13 21:20:32 +0900

    fix: retrieve top users and modify ActiveUSers component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -4,14 +4,22 @@ import { CssVarsProvider, Typography } from "@mui/joy";
 import CardOverflow from "@mui/joy/CardOverflow";
 import AspectRatio from "@mui/joy/AspectRatio";
 
-const activeUsers = [
-  { memberNick: "Martin", memberImage: "/img/martin.webp" },
-  { memberNick: "Justin", memberImage: "/img/justin.webp" },
-  { memberNick: "Rose", memberImage: "/img/rose.webp" },
-  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
-];
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveTopUsers } from "./selector";
+import { serverApi } from "../../../lib/config";
+import { Member } from "../../../lib/data/types/member";
+
+/** REDUX SLICE & SELECTOR **/
+const topUsersRetriever = createSelector(
+    retrieveTopUsers,
+    (topUsers) => ({topUsers})
+);
+
+
 
 export default function ActiveUsers() {
+  const {topUsers} = useSelector (topUsersRetriever);
   return (
     <div className={"active-users-frame"}>
       <Container>
@@ -24,17 +32,18 @@ export default function ActiveUsers() {
           flexWrap={"wrap"}
           >
             <CssVarsProvider>
-              {activeUsers.length !== 0 ? (
-                activeUsers.map((ele, index) => {
+              {topUsers.length !== 0 ? (
+                topUsers.map((member:Member ) => {
+                  const imagePath = `${serverApi}/${member.memberImage}`
                   return (
-                    <Card key={index} className={"card"}>
+                    <Card key={member._id} className={"card"}>
                       <CardOverflow>
                         <AspectRatio ratio="1">
-                          <img src={ele.memberImage} alt="" />
+                          <img src={imagePath} alt="" />
                         </AspectRatio>
                       </CardOverflow>
                       <Typography>
-                        {ele.memberNick}
+                        {member.memberNick}
                       </Typography>
                     </Card>
                   );
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,27 +9,32 @@ import "../../../css/home.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setNewDishes, setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/data/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/data/enums/product.enum";
+import MemeberService from "../../services/memberService";
+import { Member } from "../../../lib/data/types/member";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes:(data:Product[]) => dispatch(setPopularDishes(data)),// 2nd setPopularDishes is reducer
      setNewDishes:(data:Product[]) => dispatch(setNewDishes(data)),
+     setTopUsers:(data:Member[]) => dispatch(setTopUsers(data))
 });
 
 
 export  default function HomePage (){
 //Selector: Store => Data (retrieve data from Redux Store)
-const {setPopularDishes, setNewDishes} = actionDispatch(useDispatch ());
+const {setPopularDishes, setNewDishes,setTopUsers} = actionDispatch(useDispatch ());
 
 
 
 
     useEffect(() => {
         //Backend Server data request => Data(get data from backend server) => Slice:Data => Store (save data in Redux Store) => Selector: Store => Data (retrieve data from Redux Store) => Component (use data in component)
+        
+        /** Popular Dishes **/
         const product = new ProductService();
         product.getProducts({
             page:1,
@@ -43,7 +48,7 @@ const {setPopularDishes, setNewDishes} = actionDispatch(useDispatch ());
         })
         .catch((err)=> console.log(err));
 
-
+         /** Fresh Menu **/
         product.
         getProducts({
             page:1,
@@ -56,6 +61,16 @@ const {setPopularDishes, setNewDishes} = actionDispatch(useDispatch ());
             setPopularDishes(data);
         })
         .catch((err)=> console.log(err));
+
+         /** Active Users **/
+         const member = new MemeberService();
+         member
+         .getTopUsers()
+         .then ((data) => {
+            setTopUsers(data);
+         })
+         .catch(err => console.log(err));
+        
 
 
         // Slice:Data=>Store (need to save in Redux Store)
