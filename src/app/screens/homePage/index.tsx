e27364653e9c7e4f4daccca7d5d06c45fa28aa4c
Amirshokh Khalimov <amirshokhkhalimov@gmail.com>
2026-03-13 20:10:40 +0900

    feat:develop product service logic and  retrieve data from backend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,76 +9,44 @@ import "../../../css/home.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/data/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/data/enums/product.enum";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes:(data:Product[]) => dispatch(setPopularDishes(data)),// 2nd setPopularDishes is reducer
 });
-const popularDishesRetriever = createSelector(
-    retrievePopularDishes,
-    (popularDishes) => ({popularDishes})
-);
+
 
 export  default function HomePage (){
 //Selector: Store => Data (retrieve data from Redux Store)
 const {setPopularDishes} = actionDispatch(useDispatch ());
-const {popularDishes} = useSelector(popularDishesRetriever);
 
-console.log( process.env.REACT_APP_API_URL);
+
+
 
     useEffect(() => {
         //Backend Server data request => Data(get data from backend server) => Slice:Data => Store (save data in Redux Store) => Selector: Store => Data (retrieve data from Redux Store) => Component (use data in component)
-        const result = [
-            [
-    {
-        "_id": "6987fe931e8f515f08c9a862",
-        "productStatus": "PROCESS",
-        "productCollection": "DISH",
-        "productName": "Kebab",
-        "productPrice": 14,
-        "productLeftCount": 80,
-        "productSize": "NORMAL",
-        "productVolume": 1,
-        "productDesc": "This is delicious Kebab",
-        "productImages": [
-            "uploads/products/87e82563-ab1d-40be-a731-8800b1362997.jpg"
-        ],
-        "productViews": 2,
-        "createdAt": "2026-02-08T03:10:11.702Z",
-        "updatedAt": "2026-03-01T12:32:22.266Z",
-        "__v": 0
-    },
-    {
-        "_id": "69869fed95949cbdd8976049",
-        "productStatus": "PROCESS",
-        "productCollection": "DISH",
-        "productName": "Somsa",
-        "productPrice": 5,
-        "productLeftCount": 105,
-        "productSize": "NORMAL",
-        "productVolume": 1,
-        "productDesc": "qaynoq somsa",
-        "productImages": [
-            "uploads/products/a8b454d2-7531-4023-8936-56ac702bd101.jpg"
-        ],
-        "productViews": 0,
-        "createdAt": "2026-02-07T02:14:05.483Z",
-        "updatedAt": "2026-03-01T12:16:54.675Z",
-        "__v": 0
-    },
-    
-]
-        ];
+        const product = new ProductService();
+        product.getProducts({
+            page:1,
+            limit:4,
+            order:"productViews",
+            // productCollection:ProductCollection.DISH,
+        })
+        .then((data)=>{
+            // console.log("data passsed here:",data);
+            setPopularDishes(data);
+        })
+        .catch((err)=> console.log(err));
         // Slice:Data=>Store (need to save in Redux Store)
         //@ts-ignore
-        setPopularDishes (result);
+        
     }, [] );
 
-    console.log("popularDishes", popularDishes);
+    
 
     return  (
     <div className={"homepage"}>
