53f64fe806a86605e7afb40817fd36ec00c257d6
Amirshokh Khalimov <amirshokhkhalimov@gmail.com>
2026-03-13 20:42:47 +0900

    fix: retrieve fresh menu and modify new dishes componnet

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,7 +9,7 @@ import "../../../css/home.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/data/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/data/enums/product.enum";
@@ -17,12 +17,13 @@ import { ProductCollection } from "../../../lib/data/enums/product.enum";
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes:(data:Product[]) => dispatch(setPopularDishes(data)),// 2nd setPopularDishes is reducer
+     setNewDishes:(data:Product[]) => dispatch(setNewDishes(data)),
 });
 
 
 export  default function HomePage (){
 //Selector: Store => Data (retrieve data from Redux Store)
-const {setPopularDishes} = actionDispatch(useDispatch ());
+const {setPopularDishes, setNewDishes} = actionDispatch(useDispatch ());
 
 
 
@@ -38,11 +39,27 @@ const {setPopularDishes} = actionDispatch(useDispatch ());
         })
         .then((data)=>{
             // console.log("data passsed here:",data);
+            setNewDishes(data);
+        })
+        .catch((err)=> console.log(err));
+
+
+        product.
+        getProducts({
+            page:1,
+            limit:4,
+            order:"createdAt",
+            // productCollection:ProductCollection.DISH,
+        })
+        .then((data)=>{
+            // console.log("data passsed here:",data);
             setPopularDishes(data);
         })
         .catch((err)=> console.log(err));
+
+
         // Slice:Data=>Store (need to save in Redux Store)
-        //@ts-ignore
+        
         
     }, [] );
 
